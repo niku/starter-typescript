37187b48e4529d5c5bdf31e0532aae88385be25d
niku <niku@niku.name>
2020-06-03 22:41:53 +0900

    Update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
+  root: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.json",
   },
-  plugins: ["@typescript-eslint"],
-  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-  rules: {
-    // Overwrite rules specified from the extended configs e.g.
-    // "@typescript-eslint/explicit-function-return-type": "off",
+  plugins: ["jest", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  rules: {},
+  env: {
+    "jest/globals": true,
+    node: true,
   },
 };
